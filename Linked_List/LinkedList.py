31399d8d91859a11ee1b4cc788a02a8dfffb873f
John Calderaio <jcalderaio@gmail.com>
2020-09-25 16:49:22 -0400

    LinkedList (Python)

---
+++ Linked_List/LinkedList.py
@@ -0,0 +1,30 @@
+class Node:
+  def __init__(self, data):
+    self.data = data
+    self.next = None
+
+class LinkedList:
+  def __init__(self, data):
+    self.head = Node(data)
+
+  def add_node(self, data): 
+    if (self.head == None):
+      self.head = Node(data)
+      return
+
+    current = self.head
+    while (current.next != None):
+      current = current.next
+
+    current.next = Node(data)
+  
+  def print_list(self): 
+    current = self.head
+    while (current != None):
+      print(current.data)
+      current = current.next
+
+
+list = LinkedList(1)
+list.add_node(2)
+list.print_list()
